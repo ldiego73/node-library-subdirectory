bcbe90ca98fb31c6df65fbb0f68dd5e5e948900d
Luis Diego <ldiego@MacBook-Air-de-Keith.local>
2022-09-23 18:57:42 -0500

    feat: add method full name

--- libraries/node/src/index.ts
+++ libraries/node/src/index.ts
@@ -1,10 +1,15 @@
 interface NodeLibraryProps {
   name: string;
+  description: string;
   status: boolean;
 }
 
 export class NodeLibrary {
   constructor(protected readonly props: NodeLibraryProps) {}
+
+  getFullName(): string {
+    return this.props.name + this.props.description;
+  }
   
   toJSON(): Record<string, any> {
     return this.props;
